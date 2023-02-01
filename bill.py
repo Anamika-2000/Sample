5068d5a174eeaf6bae7638db08a5664542d0b620
Anamika-2000 <63454939+Anamika-2000@users.noreply.github.com>
2023-02-01 15:17:34 +0530

    my first try

---
+++ bill.py
@@ -0,0 +1,7 @@
+pr=float(input("Enter the price of the item      "))
+qn=int(input("Enter the quantity of items      "))
+bill=pr*qn
+print("The total price is    ")
+print(bill)
+no=int(input("Enter the order number"))
+print(no,bill)
